ec2e885f22082b47a641eabd643aec0dc38732a7
alvaro <alvaro@debian>
2016-03-05 20:23:52 +0100

    He modificado el ejercicio 1 del enuncidado json, porque no tiene sentido mostrar todos los vehiculos afectados en los accidentes. Lo he cambiado a mostrar los tipo de accidentes

--- solajson.py
+++ solajson.py
@@ -3,3 +3,33 @@
 
 #Ejercicio-JSON
 #ÁLVARO SOLA OLIVERO
+
+
+#1 - Lista todos los accidentes.
+
+import json
+
+with open("accidente.json") as fichero:
+	datos = json.load(fichero)
+
+print "Lista de accidentes:"
+print "--------------------------------"
+
+for x in datos["result"]["vehiculo"]:
+	print x["type"]
+
+print "--------------------------------"
+
+#2 - Muestra el número de accidentes.
+
+contador=0
+for total in datos["result"]:
+	contador=contador+1
+print "Total de accidentes:",contador
+print "--------------------------------"
+
+#3 - Pide por teclado un vehiculo y muestra cual es el tipo de accidente.
+
+#4 - Introduzca una fecha inicial y una fecha final y muestra los accidentes producidos en ese intervalo.
+
+#5 - Introduce por teclado una cadena, si esta dentro de algún comentario, muestra el comentario del accidente, sino devuelve un error. 
